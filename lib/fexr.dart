0163ad09fd44441c1db52b436dacb0b14731ae67
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-06-29 03:18:07 +0530

    simplified model structure
    Signed-off-by: Nidhin Mahesh A <nidhin.m@rubix.network>

--- lib/fexr.dart
+++ lib/fexr.dart
@@ -1,3 +1,5 @@
 library fexr;
 
-// export 'src/core/usage.dart'
+export 'package:fexr/sync-v1/pop/passport_service.dart';
+export 'package:fexr/sync-v1/pop/oracle_service.dart';
+export 'package:fexr/sync-v1/pop/pay_service.dart';
--- lib/sync-v1/pop/passport_service.dart
+++ lib/sync-v1/pop/passport_service.dart
@@ -1,3 +1,31 @@
+import 'package:grpc/grpc.dart';
+import 'package:fexr/sync-v1/protos/pop/';
+
 class PassportService {
-  
+
+  Future<void> web3WalletPermissionRequest(
+      String proxyIP, String dID, int code) async {
+    final channel = ClientChannel(
+      proxyIP,
+      port: 6942,
+      options: ChannelOptions(
+        credentials: ChannelCredentials.secure(),
+        codecRegistry:
+            CodecRegistry(codecs: const [GzipCodec(), IdentityCodec()]),
+      ),
+    );
+
+    final stub = POP(channel);
+
+    try {
+      final response = await stub.sayHello(
+        POPRequest()..name = dID,
+        options: CallOptions(compression: const GzipCodec()),
+      );
+      print('Greeter client received: ${response.message}');
+    } catch (e) {
+      print('Caught error: $e');
+    }
+    await channel.shutdown();
+  }
 }
