8635705ab40c62d22079141141e080d040304551
Allen Iype P Cherian <allen.l@rubix.network>
2022-08-16 19:10:47 +0530

    Generation and verification of sign exported.

--- lib/fexr.dart
+++ lib/fexr.dart
@@ -10,4 +10,6 @@ export 'package:fexr/sync-v1/protos/pop/pop.pbgrpc.dart';
 export 'package:fexr/sync-v1/protos/google/protobuf/empty.pb.dart';
 export 'package:fexr/sync-v1/protos/google/protobuf/empty.pbenum.dart';
 export 'package:fexr/sync-v1/protos/google/protobuf/empty.pbjson.dart';
+export 'package:fexr/signature/auth_sign.dart';
+export 'package:fexr/signature/gen_sign.dart';
 
