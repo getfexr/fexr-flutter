6a039360e21f4f0dcc6c8107b33b7e95c2499d20
Allen Iype P Cherian <allen.l@rubix.network>
2022-11-01 12:39:43 +0530

    Update proto and generated files

--- lib/fexr.dart
+++ lib/fexr.dart
@@ -1,15 +1,15 @@
 library fexr;
 
-export 'package:fexr/pop/passport_service.dart';
-export 'package:fexr/pop/oracle_service.dart';
-export 'package:fexr/pop/pay_service.dart';
-export 'package:fexr/sync-v1/protos/pop/pop.pb.dart';
-export 'package:fexr/sync-v1/protos/pop/pop.pbenum.dart';
-export 'package:fexr/sync-v1/protos/pop/pop.pbjson.dart';
-export 'package:fexr/sync-v1/protos/pop/pop.pbgrpc.dart';
-export 'package:fexr/sync-v1/protos/google/protobuf/empty.pb.dart';
-export 'package:fexr/sync-v1/protos/google/protobuf/empty.pbenum.dart';
-export 'package:fexr/sync-v1/protos/google/protobuf/empty.pbjson.dart';
+export 'package:fexr/protogen/sky.pb.dart';
+export 'package:fexr/protogen/sky.pbenum.dart';
+export 'package:fexr/protogen/sky.pbgrpc.dart';
+export 'package:fexr/protogen/sky.pbjson.dart';
+export 'package:fexr/protogen/google/protobuf/empty.pb.dart';
+export 'package:fexr/protogen/google/protobuf/empty.pbenum.dart';
+export 'package:fexr/protogen/google/protobuf/empty.pbjson.dart';
+export 'package:fexr/protogen/google/protobuf/timestamp.pb.dart';
+export 'package:fexr/protogen/google/protobuf/timestamp.pbjson.dart';
+export 'package:fexr/protogen/google/protobuf/timestamp.pbenum.dart';
 export 'package:fexr/signature/auth_sign.dart';
 export 'package:fexr/signature/gen_sign.dart';
 export 'package:fexr/signature/sign_model.dart';
