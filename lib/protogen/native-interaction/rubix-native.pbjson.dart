4e93d5ebe9b1971dff83ec11bc51ade90c8c0b03
Allen Iype P Cherian <allen.l@rubix.network>
2022-11-08 01:49:07 +0530

    Fix txndetails

--- lib/protogen/native-interaction/rubix-native.pbjson.dart
+++ lib/protogen/native-interaction/rubix-native.pbjson.dart
@@ -43,30 +43,20 @@ const RequestTransactionPayloadReq$json = const {
 
 /// Descriptor for `RequestTransactionPayloadReq`. Decode as a `google.protobuf.DescriptorProto`.
 final $typed_data.Uint8List requestTransactionPayloadReqDescriptor = $convert.base64Decode('ChxSZXF1ZXN0VHJhbnNhY3Rpb25QYXlsb2FkUmVxEhoKCHJlY2VpdmVyGAEgASgJUghyZWNlaXZlchIeCgp0b2tlbkNvdW50GAIgASgBUgp0b2tlbkNvdW50EhgKB2NvbW1lbnQYAyABKAlSB2NvbW1lbnQSEgoEdHlwZRgEIAEoBVIEdHlwZQ==');
-@$core.Deprecated('Use txnDetailsDescriptor instead')
-const TxnDetails$json = const {
-  '1': 'TxnDetails',
+@$core.Deprecated('Use txnSummaryDescriptor instead')
+const TxnSummary$json = const {
+  '1': 'TxnSummary',
   '2': const [
-    const {'1': 'wholeTokens', '3': 1, '4': 1, '5': 9, '10': 'wholeTokens'},
-    const {'1': 'wholeTokenChainHash', '3': 2, '4': 1, '5': 9, '10': 'wholeTokenChainHash'},
-    const {'1': 'partTokenChainHash', '3': 3, '4': 1, '5': 9, '10': 'partTokenChainHash'},
-    const {'1': 'partTokens', '3': 4, '4': 1, '5': 9, '10': 'partTokens'},
-    const {'1': 'partTokenChainArrays', '3': 5, '4': 1, '5': 9, '10': 'partTokenChainArrays'},
-    const {'1': 'amountLedger', '3': 6, '4': 1, '5': 9, '10': 'amountLedger'},
-    const {'1': 'tokenPreviousSender', '3': 7, '4': 1, '5': 9, '10': 'tokenPreviousSender'},
-    const {'1': 'doubleSpendString', '3': 8, '4': 1, '5': 9, '10': 'doubleSpendString'},
-    const {'1': 'receiverDidIpfsHash', '3': 9, '4': 1, '5': 9, '10': 'receiverDidIpfsHash'},
-    const {'1': 'requestedAmount', '3': 10, '4': 1, '5': 1, '10': 'requestedAmount'},
-    const {'1': 'comment', '3': 11, '4': 1, '5': 9, '10': 'comment'},
-    const {'1': 'tid', '3': 12, '4': 1, '5': 9, '10': 'tid'},
-    const {'1': 'allTokens', '3': 13, '4': 1, '5': 9, '10': 'allTokens'},
-    const {'1': 'newPart', '3': 14, '4': 1, '5': 8, '10': 'newPart'},
-    const {'1': 'oldPart', '3': 15, '4': 1, '5': 8, '10': 'oldPart'},
+    const {'1': 'txnId', '3': 1, '4': 1, '5': 9, '10': 'txnId'},
+    const {'1': 'sender', '3': 2, '4': 1, '5': 9, '10': 'sender'},
+    const {'1': 'receiver', '3': 3, '4': 1, '5': 9, '10': 'receiver'},
+    const {'1': 'amount', '3': 4, '4': 1, '5': 1, '10': 'amount'},
+    const {'1': 'comment', '3': 5, '4': 1, '5': 9, '10': 'comment'},
   ],
 };
 
-/// Descriptor for `TxnDetails`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List txnDetailsDescriptor = $convert.base64Decode('CgpUeG5EZXRhaWxzEiAKC3dob2xlVG9rZW5zGAEgASgJUgt3aG9sZVRva2VucxIwChN3aG9sZVRva2VuQ2hhaW5IYXNoGAIgASgJUhN3aG9sZVRva2VuQ2hhaW5IYXNoEi4KEnBhcnRUb2tlbkNoYWluSGFzaBgDIAEoCVIScGFydFRva2VuQ2hhaW5IYXNoEh4KCnBhcnRUb2tlbnMYBCABKAlSCnBhcnRUb2tlbnMSMgoUcGFydFRva2VuQ2hhaW5BcnJheXMYBSABKAlSFHBhcnRUb2tlbkNoYWluQXJyYXlzEiIKDGFtb3VudExlZGdlchgGIAEoCVIMYW1vdW50TGVkZ2VyEjAKE3Rva2VuUHJldmlvdXNTZW5kZXIYByABKAlSE3Rva2VuUHJldmlvdXNTZW5kZXISLAoRZG91YmxlU3BlbmRTdHJpbmcYCCABKAlSEWRvdWJsZVNwZW5kU3RyaW5nEjAKE3JlY2VpdmVyRGlkSXBmc0hhc2gYCSABKAlSE3JlY2VpdmVyRGlkSXBmc0hhc2gSKAoPcmVxdWVzdGVkQW1vdW50GAogASgBUg9yZXF1ZXN0ZWRBbW91bnQSGAoHY29tbWVudBgLIAEoCVIHY29tbWVudBIQCgN0aWQYDCABKAlSA3RpZBIcCglhbGxUb2tlbnMYDSABKAlSCWFsbFRva2VucxIYCgduZXdQYXJ0GA4gASgIUgduZXdQYXJ0EhgKB29sZFBhcnQYDyABKAhSB29sZFBhcnQ=');
+/// Descriptor for `TxnSummary`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List txnSummaryDescriptor = $convert.base64Decode('CgpUeG5TdW1tYXJ5EhQKBXR4bklkGAEgASgJUgV0eG5JZBIWCgZzZW5kZXIYAiABKAlSBnNlbmRlchIaCghyZWNlaXZlchgDIAEoCVIIcmVjZWl2ZXISFgoGYW1vdW50GAQgASgBUgZhbW91bnQSGAoHY29tbWVudBgFIAEoCVIHY29tbWVudA==');
 @$core.Deprecated('Use requestTransactionPayloadResDescriptor instead')
 const RequestTransactionPayloadRes$json = const {
   '1': 'RequestTransactionPayloadRes',
@@ -74,6 +64,7 @@ const RequestTransactionPayloadRes$json = const {
     const {'1': 'did', '3': 1, '4': 1, '5': 9, '10': 'did'},
     const {'1': 'tid', '3': 2, '4': 1, '5': 9, '10': 'tid'},
     const {'1': 'payload', '3': 3, '4': 1, '5': 11, '6': '.protos.RequestTransactionPayloadRes.TxnSignPayload', '10': 'payload'},
+    const {'1': 'txnSummary', '3': 4, '4': 1, '5': 11, '6': '.protos.TxnSummary', '10': 'txnSummary'},
   ],
   '3': const [RequestTransactionPayloadRes_TxnSignPayload$json],
 };
@@ -84,12 +75,12 @@ const RequestTransactionPayloadRes_TxnSignPayload$json = const {
   '2': const [
     const {'1': 'senderSign', '3': 1, '4': 1, '5': 9, '10': 'senderSign'},
     const {'1': 'senderSignQ', '3': 2, '4': 1, '5': 9, '10': 'senderSignQ'},
-    const {'1': 'txnDetails', '3': 3, '4': 1, '5': 11, '6': '.protos.TxnDetails', '10': 'txnDetails'},
+    const {'1': 'txnDetails', '3': 3, '4': 1, '5': 9, '10': 'txnDetails'},
   ],
 };
 
 /// Descriptor for `RequestTransactionPayloadRes`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List requestTransactionPayloadResDescriptor = $convert.base64Decode('ChxSZXF1ZXN0VHJhbnNhY3Rpb25QYXlsb2FkUmVzEhAKA2RpZBgBIAEoCVIDZGlkEhAKA3RpZBgCIAEoCVIDdGlkEk0KB3BheWxvYWQYAyABKAsyMy5wcm90b3MuUmVxdWVzdFRyYW5zYWN0aW9uUGF5bG9hZFJlcy5UeG5TaWduUGF5bG9hZFIHcGF5bG9hZBqGAQoOVHhuU2lnblBheWxvYWQSHgoKc2VuZGVyU2lnbhgBIAEoCVIKc2VuZGVyU2lnbhIgCgtzZW5kZXJTaWduURgCIAEoCVILc2VuZGVyU2lnblESMgoKdHhuRGV0YWlscxgDIAEoCzISLnByb3Rvcy5UeG5EZXRhaWxzUgp0eG5EZXRhaWxz');
+final $typed_data.Uint8List requestTransactionPayloadResDescriptor = $convert.base64Decode('ChxSZXF1ZXN0VHJhbnNhY3Rpb25QYXlsb2FkUmVzEhAKA2RpZBgBIAEoCVIDZGlkEhAKA3RpZBgCIAEoCVIDdGlkEk0KB3BheWxvYWQYAyABKAsyMy5wcm90b3MuUmVxdWVzdFRyYW5zYWN0aW9uUGF5bG9hZFJlcy5UeG5TaWduUGF5bG9hZFIHcGF5bG9hZBIyCgp0eG5TdW1tYXJ5GAQgASgLMhIucHJvdG9zLlR4blN1bW1hcnlSCnR4blN1bW1hcnkacgoOVHhuU2lnblBheWxvYWQSHgoKc2VuZGVyU2lnbhgBIAEoCVIKc2VuZGVyU2lnbhIgCgtzZW5kZXJTaWduURgCIAEoCVILc2VuZGVyU2lnblESHgoKdHhuRGV0YWlscxgDIAEoCVIKdHhuRGV0YWlscw==');
 @$core.Deprecated('Use initiateTransactionReqDescriptor instead')
 const InitiateTransactionReq$json = const {
   '1': 'InitiateTransactionReq',
@@ -109,7 +100,7 @@ const InitiateTransactionReq_SignedTransactionPayload$json = const {
     const {'1': 'positions', '3': 1, '4': 1, '5': 9, '10': 'positions'},
     const {'1': 'senderSign', '3': 2, '4': 1, '5': 11, '6': '.protos.InitiateTransactionReq.SignedTransactionPayload.SignedContent', '10': 'senderSign'},
     const {'1': 'senderSignQ', '3': 3, '4': 1, '5': 11, '6': '.protos.InitiateTransactionReq.SignedTransactionPayload.SignedContent', '10': 'senderSignQ'},
-    const {'1': 'txnDetails', '3': 4, '4': 1, '5': 11, '6': '.protos.TxnDetails', '10': 'txnDetails'},
+    const {'1': 'txnDetails', '3': 4, '4': 1, '5': 9, '10': 'txnDetails'},
   ],
   '3': const [InitiateTransactionReq_SignedTransactionPayload_SignedContent$json],
 };
@@ -124,7 +115,7 @@ const InitiateTransactionReq_SignedTransactionPayload_SignedContent$json = const
 };
 
 /// Descriptor for `InitiateTransactionReq`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List initiateTransactionReqDescriptor = $convert.base64Decode('ChZJbml0aWF0ZVRyYW5zYWN0aW9uUmVxEhoKCHJlY2VpdmVyGAEgASgJUghyZWNlaXZlchIeCgp0b2tlbkNvdW50GAIgASgBUgp0b2tlbkNvdW50EhIKBHR5cGUYAyABKAVSBHR5cGUSXQoNcGF5bG9hZFNpZ25lZBgEIAEoCzI3LnByb3Rvcy5Jbml0aWF0ZVRyYW5zYWN0aW9uUmVxLlNpZ25lZFRyYW5zYWN0aW9uUGF5bG9hZFINcGF5bG9hZFNpZ25lZBqFAwoYU2lnbmVkVHJhbnNhY3Rpb25QYXlsb2FkEhwKCXBvc2l0aW9ucxgBIAEoCVIJcG9zaXRpb25zEmUKCnNlbmRlclNpZ24YAiABKAsyRS5wcm90b3MuSW5pdGlhdGVUcmFuc2FjdGlvblJlcS5TaWduZWRUcmFuc2FjdGlvblBheWxvYWQuU2lnbmVkQ29udGVudFIKc2VuZGVyU2lnbhJnCgtzZW5kZXJTaWduURgDIAEoCzJFLnByb3Rvcy5Jbml0aWF0ZVRyYW5zYWN0aW9uUmVxLlNpZ25lZFRyYW5zYWN0aW9uUGF5bG9hZC5TaWduZWRDb250ZW50UgtzZW5kZXJTaWduURIyCgp0eG5EZXRhaWxzGAQgASgLMhIucHJvdG9zLlR4bkRldGFpbHNSCnR4bkRldGFpbHMaRwoNU2lnbmVkQ29udGVudBIYCgdjb250ZW50GAEgASgJUgdjb250ZW50EhwKCXNpZ25hdHVyZRgCIAEoCVIJc2lnbmF0dXJl');
+final $typed_data.Uint8List initiateTransactionReqDescriptor = $convert.base64Decode('ChZJbml0aWF0ZVRyYW5zYWN0aW9uUmVxEhoKCHJlY2VpdmVyGAEgASgJUghyZWNlaXZlchIeCgp0b2tlbkNvdW50GAIgASgBUgp0b2tlbkNvdW50EhIKBHR5cGUYAyABKAVSBHR5cGUSXQoNcGF5bG9hZFNpZ25lZBgEIAEoCzI3LnByb3Rvcy5Jbml0aWF0ZVRyYW5zYWN0aW9uUmVxLlNpZ25lZFRyYW5zYWN0aW9uUGF5bG9hZFINcGF5bG9hZFNpZ25lZBrxAgoYU2lnbmVkVHJhbnNhY3Rpb25QYXlsb2FkEhwKCXBvc2l0aW9ucxgBIAEoCVIJcG9zaXRpb25zEmUKCnNlbmRlclNpZ24YAiABKAsyRS5wcm90b3MuSW5pdGlhdGVUcmFuc2FjdGlvblJlcS5TaWduZWRUcmFuc2FjdGlvblBheWxvYWQuU2lnbmVkQ29udGVudFIKc2VuZGVyU2lnbhJnCgtzZW5kZXJTaWduURgDIAEoCzJFLnByb3Rvcy5Jbml0aWF0ZVRyYW5zYWN0aW9uUmVxLlNpZ25lZFRyYW5zYWN0aW9uUGF5bG9hZC5TaWduZWRDb250ZW50UgtzZW5kZXJTaWduURIeCgp0eG5EZXRhaWxzGAQgASgJUgp0eG5EZXRhaWxzGkcKDVNpZ25lZENvbnRlbnQSGAoHY29udGVudBgBIAEoCVIHY29udGVudBIcCglzaWduYXR1cmUYAiABKAlSCXNpZ25hdHVyZQ==');
 @$core.Deprecated('Use initiateTransactionResDescriptor instead')
 const InitiateTransactionRes$json = const {
   '1': 'InitiateTransactionRes',
--- lib/rpc/native-interaction/rubix_service.dart
+++ lib/rpc/native-interaction/rubix_service.dart
@@ -45,22 +45,7 @@ class RubixService {
             positions: initiatePayload.payloadSigned.positions,
             senderSign:InitiateTransactionReq_SignedTransactionPayload_SignedContent(content:initiatePayload.payloadSigned.senderSign.content , signature: await GenerateSign().genSignFromShares(imagePath, Dependencies().calculateHash(initiatePayload.payloadSigned.senderSign.content))),
             senderSignQ: InitiateTransactionReq_SignedTransactionPayload_SignedContent(content: initiatePayload.payloadSigned.senderSignQ.content, signature: await GenerateSign().genSignFromShares(imagePath, Dependencies().calculateHash(initiatePayload.payloadSigned.senderSignQ.content))),
-            txnDetails: TxnDetails(wholeTokens: initiatePayload.payloadSigned.txnDetails.wholeTokens,
-            wholeTokenChainHash: initiatePayload.payloadSigned.txnDetails.wholeTokenChainHash,
-            partTokenChainHash: initiatePayload.payloadSigned.txnDetails.partTokenChainHash,
-            partTokens: initiatePayload.payloadSigned.txnDetails.partTokens,
-            partTokenChainArrays: initiatePayload.payloadSigned.txnDetails.partTokenChainArrays,
-            amountLedger: initiatePayload.payloadSigned.txnDetails.amountLedger,
-            tokenPreviousSender: initiatePayload.payloadSigned.txnDetails.tokenPreviousSender,
-            doubleSpendString: initiatePayload.payloadSigned.txnDetails.doubleSpendString,
-            receiverDidIpfsHash: initiatePayload.payloadSigned.txnDetails.receiverDidIpfsHash,
-            requestedAmount: initiatePayload.payloadSigned.txnDetails.requestedAmount,
-            comment: initiatePayload.payloadSigned.txnDetails.comment,
-            tid: initiatePayload.payloadSigned.txnDetails.tid,
-            newPart: initiatePayload.payloadSigned.txnDetails.newPart,
-            oldPart: initiatePayload.payloadSigned.txnDetails.oldPart
-            
-            )
+            txnDetails: initiatePayload.payloadSigned.txnDetails
           )
       )
       );
