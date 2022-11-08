5045c44a237aa87c24bedc57dfdc4a095030fc07
Nidhin Mahesh A <nidhinmahesh1@gmail.com>
2022-11-08 20:13:18 +0530

    sign txn for rubix
    Signed-off-by: Nidhin Mahesh A <nidhinmahesh1@gmail.com>

--- lib/rpc/native-interaction/rubix_service.dart
+++ lib/rpc/native-interaction/rubix_service.dart
@@ -4,9 +4,37 @@ import 'package:fexr/protogen/native-interaction/rubix-native.pbgrpc.dart';
 import 'package:fexr/signature/dependencies.dart';
 import 'package:grpc/grpc.dart';
 
-
-
 class RubixService {
+  Future<TxnSummary> signTransaction(
+      {required String gateway,
+      required double amount,
+      required String accessToken,
+      required String privateImagePath,
+      String? comment,
+      int? type,
+      List<String>? quorumlist,
+      required String receiver}) async {
+      RequestTransactionPayloadRes payload = await requestTransactionPayload(
+        accessToken: accessToken,
+        gateway: gateway,
+        payloadReq: RequestTransactionPayloadReq(
+          comment: comment,
+          type: type,
+          receiver: receiver,
+        ));
+
+        String positions = await 
+        String senderSign = await
+        String senderSignQ = await
+
+        TxnSummary txnSummary = await initiateTransaction(
+          accessToken: accessToken,
+          gateway: gateway, imagePath: privateImagePath, initiatePayload: InitiateTransactionReq(receiver: receiver, tokenCount: amount, type: type, payloadSigned: InitiateTransactionReq_SignedTransactionPayload(positions: positions, senderSign: senderSign, senderSignQ: senderSignQ, txnDetails: '')),
+        );
+
+        return txnSummary;
+  }
+
   static final RubixService _singleton = RubixService._internal();
 
   factory RubixService() {
@@ -15,72 +43,82 @@ class RubixService {
 
   RubixService._internal();
 
+  RubixServiceClient getConnection(String gateway, String accessToken) {
+    ClientChannel channel = ClientChannel(gateway,
+        port: Const.PORT,
+        options:
+            const ChannelOptions(credentials: ChannelCredentials.insecure()));
+    return RubixServiceClient(channel,
+        options:
+            CallOptions(metadata: {'Authorization': 'Bearer $accessToken'}));
+  }
 
-   RubixServiceClient getConnection(String gateway, String accessToken){
-      ClientChannel channel = ClientChannel(gateway,
-          port: Const.PORT,
-          options: const ChannelOptions(
-              credentials: ChannelCredentials.insecure()));
-       return RubixServiceClient(channel, options: CallOptions(metadata: {
-      'Authorization': 'Bearer $accessToken'
-    }));
-  
-   }
-
-  Future<CreateDIDRes> createDID({required String gateway, required String didImagePath, required String publicSharePath, required String accessToken}) async {
-    RubixServiceClient stub = getConnection(gateway,accessToken);
+  Future<CreateDIDRes> createDID(
+      {required String gateway,
+      required String didImagePath,
+      required String publicSharePath,
+      required String accessToken}) async {
+    RubixServiceClient stub = getConnection(gateway, accessToken);
     try {
-      var response = await stub.createDID(
-        CreateDIDReq(
+      var response = await stub.createDID(CreateDIDReq(
           didImage: await Dependencies().imageToBase64(didImagePath),
-          publicShare: await Dependencies().imageToBase64(publicSharePath)
-
-        ));
-    return response;
+          publicShare: await Dependencies().imageToBase64(publicSharePath)));
+      return response;
     } catch (e) {
       print(e);
       return CreateDIDRes();
     }
-
   }
 
-  Future<TxnSummary> initiateTransaction ({required String gateway, required String accessToken, required InitiateTransactionReq initiatePayload, required String imagePath }) async {
-    RubixServiceClient stub = getConnection(gateway,accessToken);
+  Future<TxnSummary> initiateTransaction(
+      {required String gateway,
+      required String accessToken,
+      required InitiateTransactionReq initiatePayload,
+      required String imagePath}) async {
+    RubixServiceClient stub = getConnection(gateway, accessToken);
     try {
-      var response = await stub.initiateTransaction(
-        InitiateTransactionReq(
+      var response = await stub.initiateTransaction(InitiateTransactionReq(
           payloadSigned: InitiateTransactionReq_SignedTransactionPayload(
-            positions: initiatePayload.payloadSigned.positions,
-            senderSign:InitiateTransactionReq_SignedTransactionPayload_SignedContent(content:initiatePayload.payloadSigned.senderSign.content , signature: await GenerateSign().genSignFromShares(imagePath, Dependencies().calculateHash(initiatePayload.payloadSigned.senderSign.content))),
-            senderSignQ: InitiateTransactionReq_SignedTransactionPayload_SignedContent(content: initiatePayload.payloadSigned.senderSignQ.content, signature: await GenerateSign().genSignFromShares(imagePath, Dependencies().calculateHash(initiatePayload.payloadSigned.senderSignQ.content))),
-            txnDetails: initiatePayload.payloadSigned.txnDetails
-          )
-      )
-      );
+              positions: initiatePayload.payloadSigned.positions,
+              senderSign:
+                  InitiateTransactionReq_SignedTransactionPayload_SignedContent(
+                      content: initiatePayload.payloadSigned.senderSign.content,
+                      signature: await GenerateSign().genSignFromShares(
+                          imagePath,
+                          Dependencies().calculateHash(initiatePayload
+                              .payloadSigned.senderSign.content))),
+              senderSignQ:
+                  InitiateTransactionReq_SignedTransactionPayload_SignedContent(
+                      content:
+                          initiatePayload.payloadSigned.senderSignQ.content,
+                      signature: await GenerateSign().genSignFromShares(
+                          imagePath,
+                          Dependencies().calculateHash(
+                              initiatePayload.payloadSigned.senderSignQ.content))),
+              txnDetails: initiatePayload.payloadSigned.txnDetails)));
       return response;
     } catch (e) {
       print(e);
       return TxnSummary();
     }
-}
+  }
 
-  Future<RequestTransactionPayloadRes> requestTransactionPayload({required String gateway, required String accessToken, required RequestTransactionPayloadReq payloadReq}) async {
-    RubixServiceClient stub = getConnection(gateway,accessToken);
+  Future<RequestTransactionPayloadRes> requestTransactionPayload(
+      {required String gateway,
+      required String accessToken,
+      required RequestTransactionPayloadReq payloadReq}) async {
+    RubixServiceClient stub = getConnection(gateway, accessToken);
     try {
       var response = await stub.requestTransactionPayload(
-        RequestTransactionPayloadReq(
-          receiver: payloadReq.receiver,
-          tokenCount: payloadReq.tokenCount,
-          comment: payloadReq.comment,
-          type: payloadReq.type
-        )
-      );
+          RequestTransactionPayloadReq(
+              receiver: payloadReq.receiver,
+              tokenCount: payloadReq.tokenCount,
+              comment: payloadReq.comment,
+              type: payloadReq.type));
       return response;
     } catch (e) {
       print(e);
       return RequestTransactionPayloadRes();
-    } 
-
+    }
   }
-  
 }
