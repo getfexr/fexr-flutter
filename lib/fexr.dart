b2f5d368486f27346fb6ef65c823f5c7b64564a9
Allen Iype P Cherian <allen.l@rubix.network>
2023-04-03 13:11:42 +0530

    Update export RubixExternalService

--- lib/fexr.dart
+++ lib/fexr.dart
@@ -1,23 +1,12 @@
 library fexr;
 
 export 'package:fexr/protogen/google/protobuf/empty.pb.dart';
-export 'package:fexr/protogen/google/protobuf/empty.pbenum.dart';
-export 'package:fexr/protogen/google/protobuf/empty.pbjson.dart';
 export 'package:fexr/protogen/google/protobuf/timestamp.pb.dart';
-export 'package:fexr/protogen/google/protobuf/timestamp.pbjson.dart';
-export 'package:fexr/protogen/google/protobuf/timestamp.pbenum.dart';
 export 'package:fexr/signature/auth_sign.dart';
 export 'package:fexr/signature/gen_sign.dart';
 export 'package:fexr/signature/sign_model.dart';
 export 'package:fexr/components/buttons.dart';
 export 'package:fexr/rpc/native-interaction/rubix_service.dart';
-export 'package:fexr/protogen/native-interaction/rubix-native.pb.dart';
-export 'package:fexr/protogen/native-interaction/rubix-native.pbenum.dart';
-export 'package:fexr/protogen/native-interaction/rubix-native.pbgrpc.dart';
-export 'package:fexr/protogen/native-interaction/rubix-native.pbjson.dart';
-export 'package:fexr/protogen/native-interaction/rubix-external.pb.dart';
-export 'package:fexr/protogen/native-interaction/rubix-external.pbenum.dart';
-export 'package:fexr/protogen/native-interaction/rubix-external.pbgrpc.dart';
-export 'package:fexr/protogen/native-interaction/rubix-external.pbjson.dart';
 export 'package:fexr/signature/key_gen.dart';
+export 'package:fexr/rpc/native-interaction/rubix_external.dart';
 
